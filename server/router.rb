564520c3a3bcca260941d569e672dda51d555c81
Michael Prather <mprather@skynet.shintech.ninja>
2018-03-03 05:27:43 +0000

    crud

--- server/router.rb
+++ server/router.rb
@@ -12,6 +12,47 @@ get '/models' do
   }.to_json  
 end
 
+get '/models/:id' do
+  content_type :json
+  { :body => {
+      :results => Model.find(params[:id])
+    }
+  }.to_json
+end
+
+post '/models' do 
+  @model = Model.create({:name => request[:name]})
+  
+  content_type :json
+  
+  { :body => {
+      :results => 'create'
+    }
+  }.to_json  
+end
+
+put '/models/:id' do 
+  @model = Model.find(params[:id])
+  
+  content_type :json
+  
+  { :body => {
+      :results => 'update'
+    }
+  }.to_json    
+end
+
+delete '/models/:id' do 
+  Model.destroy(params[:id])
+  
+  content_type :json
+  
+  { :body => {
+      :results => 'delete'
+    }
+  }.to_json    
+end
+
 get "/assets/*" do
   env["PATH_INFO"].sub!("/assets", "")
   puts 'info ' + env["PATH_INFO"]
