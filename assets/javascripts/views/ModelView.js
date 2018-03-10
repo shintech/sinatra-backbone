a8eeceb26ce5dcc6baf9bfa3f0efdd733bbbdb2b
Michael Prather <mike@skynet.shintech.ninja>
2018-03-10 06:38:45 +0000

    add an edit button to model view and get data-id when clicked

--- assets/javascripts/views/ModelView.js
+++ assets/javascripts/views/ModelView.js
@@ -1,6 +1,22 @@
 const ModelView = Backbone.Marionette.View.extend({
   tagName: 'li',
-  template: require('../templates/model-view-template.html')
+
+  template: require('../templates/model-view-template.html'),
+
+  events: {
+    'click button': 'handleClick'
+  },
+
+  attributes: function () {
+    return {
+      'data-id': this.model.get('id')
+    }
+  },
+
+  handleClick: function (e) {
+    const id = $(e.currentTarget).parent().data('id')
+    console.log(id)
+  }
 })
 
 export default ModelView
